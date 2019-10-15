49772f837e302f75d3027aa38541114e7887302f
Sawutjan <laqin3@gmail.com>
2019-10-15 00:24:12 -0400

    American guy commit

--- src/test/java/qaclickacademy/RestAPITest.java
+++ src/test/java/qaclickacademy/RestAPITest.java
@@ -10,6 +10,15 @@ public class RestAPITest {
 		System.out.println("postJira");
 		System.out.println("postJira2");	
 		System.out.println("postJira2");
+		System.out.println("postJira4");
+		System.out.println("postJira5");
+		
+		System.out.println("postJira6");
+		System.out.println("postJira7");
+		System.out.println("postJira8");
+		//fsadjfs
+		//ghhe
+		//gdes
 	}
 	
 	@Test
